f4fdd01e8be286aaef6f538e1b50e4521f71abcb
leehanseock <leehanseock@gmail.com>
2024-09-05 15:31:20 +0900

    React 4장

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './chp03/Book.css'
+import './chp04/Button.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from "./chp03/Library"
+import ConfirmDialog from "./chp04/ConfirmDialog";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library />
+    <ConfirmDialog/>
   </React.StrictMode>
 );
 
